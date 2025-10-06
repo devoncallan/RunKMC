057a647d57e392727a9b4ff44ddbe381e55c6c0b
Devon <devoncallan@ucsb.edu>
2025-10-06 16:11:30 -0700

    FRP1 template

--- runkmc/models/__init__.py
+++ runkmc/models/__init__.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from runkmc import PATHS
 
 REGISTERED_MODELS = {
+    "FRP1": PATHS.TEMPLATE_DIR / "homo/FRP1_Template.txt",
     "FRP2": PATHS.TEMPLATE_DIR / "binary/FRP2_Template.txt",
     "CRP1": PATHS.TEMPLATE_DIR / "binary/CRP1_Template.txt",
     "CRP3": PATHS.TEMPLATE_DIR / "binary/CRP3_Template.txt",
@@ -12,12 +13,18 @@ REGISTERED_MODELS = {
 
 
 def create_input_file(
-    model_name: str, kmc_inputs: Dict[str, Any], filepath: Path | str
+    model_name: str | Path, kmc_inputs: Dict[str, Any], filepath: Path | str
 ) -> None:
 
-    model_filepath = REGISTERED_MODELS.get(model_name)
-    if model_filepath is None:
+    if isinstance(model_name, str) and model_name in REGISTERED_MODELS:
+        model_filepath = REGISTERED_MODELS.get(model_name)
+    elif Path(model_name).exists():
+        model_filepath = Path(model_name)
+    else:
         raise ValueError(f"Model {model_name} not supported")
+    
+    if not model_filepath or not model_filepath.exists():
+        raise FileNotFoundError(f"Model template file {model_filepath} not found.")
 
     # Read model template file
     with open(model_filepath, "r") as file:
